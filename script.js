08a06511ad3123fdd12d00e6b040ba35f9655087
Abdelrhman <abdelrazkforman966@gmail.com>
2026-05-22 22:09:43 +0300

    improving the interface of the website by making some glowing stars

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ button.addEventListener("click", () => {
     }
     output.innerHTML = `"${input}"`;
 });
+
+const starscontainer = document.querySelector(".stars");
+for(let i =0; i < 100; i++){
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.style.top = Math.random() * 100 + "%";
+    star.style.left = Math.random() * 100 + "%";
+    star.style.animationDuration = (Math.random() * 3 +2) + "s";
+    starscontainer.appendChild(star);
+}
